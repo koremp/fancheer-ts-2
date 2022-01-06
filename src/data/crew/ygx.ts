607ba1bca3f924f890e86839288d7c77c63a0c2f
Dokyun Lim <im.koremp@gmail.com>
2022-01-06 11:47:13 +0900

    Update YGX Crew Data

---
+++ src/data/crew/ygx.ts
@@ -0,0 +1,48 @@
+import Crew from 'data/type/Crew'
+import Dancer from 'data/type/Dancer'
+
+const Yeri: Dancer = {
+  name: {
+    kor: '예리',
+    eng: 'YELL',
+  },
+  image: '',
+  birth: new Date(2000, 3, 23),
+  description: '',
+  choreo: [
+    // TBD
+  ],
+  link: [
+    // TBD
+  ]
+}
+
+const LeeJung: Dancer = {
+  name: {
+    kor: '이이정',
+    eng: 'LEEJUNG',
+  },
+  image: '',
+  birth: new Date(1998, 8, 8),
+  isLeader: true,
+  description: '',
+  choreo: [
+    // TBD
+  ],
+  link: [
+    // TBD
+  ]
+}
+
+const YGX: Crew = {
+  name: {
+    kor: '와이지엑스',
+    eng: 'YGX'
+  },
+  image: '',
+  description: '',
+  finalRank: 5,
+  members: [
+
+  ],
+}
