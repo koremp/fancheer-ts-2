722abebac4d2e73356d5088fd32061094ada9023
Dokyun Lim <im.koremp@gmail.com>
2022-01-07 19:25:21 +0900

    Change Header Menu to Crew Data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { Layout, Menu } from 'antd'
-import { crewData } from 'data/crew'
+import crewData from 'datas/crews'
+import CREW from 'datas/types/Crew'
 
 const { Header, Content, Sider, Footer } = Layout
 
 const Main = () => {
-  const [selectedCrew, setSelectedCrew] = useState(crewData[0])
+  const [selectedCrew, setSelectedCrew] = useState<CREW>(crewData[0])
 
   return (
     <Layout >
@@ -13,17 +14,9 @@ const Main = () => {
         <Menu theme="dark" mode="horizontal" selectedKeys={[selectedCrew.name.eng]}>
           {
             crewData.map((crew) => (
-              <Menu.Item key={crew.name.eng}>{crew.name.kor}</Menu.Item>
+              <Menu.Item key={crew.name.eng}>{crew.name.eng}</Menu.Item>
             ))
           }
-          {/* <Menu.Item>YGX</Menu.Item>
-          <Menu.Item>LACHICA</Menu.Item>
-          <Menu.Item>WANT</Menu.Item>
-          <Menu.Item>WAYB</Menu.Item>
-          <Menu.Item>CocaNButter</Menu.Item>
-          <Menu.Item>PROWDMON</Menu.Item>
-          <Menu.Item>HolyBang</Menu.Item>
-          <Menu.Item>Hook</Menu.Item> */}
         </Menu>
       </Header>
       <Layout hasSider>
