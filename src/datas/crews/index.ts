7372ad94d1f1af74d0ff67cff8cc2d3e519d4903
Dokyun Lim <im.koremp@gmail.com>
2022-01-07 19:10:47 +0900

    Rename Crew Data Path Alias

---
+++ src/datas/crews/index.ts
@@ -0,0 +1,23 @@
+import HolyBang from 'datas/crews/holybang'
+import HOOK from 'datas/crews/hook'
+import LACHICA from 'datas/crews/lachica'
+import COCANBUTTER from 'datas/crews/cocanbutter'
+import YGX from 'datas/crews/ygx'
+import PROWDMON from 'data/crews/prowdmon'
+import WANT from 'datas/crews/want'
+import WAYB from 'datas/crews/wayb'
+
+import CREW from 'datas/types/Crew'
+
+const crewData: CREW[] = [
+  HolyBang, 
+  HOOK,
+  LACHICA,
+  COCANBUTTER,
+  YGX,
+  PROWDMON,
+  WANT,
+  WAYB,
+]
+
+export default crewData
