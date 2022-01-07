447e924f1814c207ddcf2f3b2407057547df4588
Dokyun Lim <im.koremp@gmail.com>
2022-01-07 16:23:39 +0900

    Add YGX Crew Data

--- src/data/crew/ygx.ts
+++ src/data/crew/ygx.ts
@@ -1,14 +1,14 @@
 import Crew from 'data/type/Crew'
 import Dancer from 'data/type/Dancer'
 
-const Yeri: Dancer = {
+const Yell: Dancer = {
   name: {
     kor: '예리',
-    eng: 'YELL',
+    eng: 'Yell',
   },
   image: '',
   birth: new Date(2000, 3, 23),
-  description: '',
+  description: '2018 Summer Youth Olympics B-Girl 1v1 Bronze Medallist',
   choreo: [
     // TBD
   ],
@@ -17,12 +17,12 @@ const Yeri: Dancer = {
   ]
 }
 
-const LeeJung: Dancer = {
+const Leejung: Dancer = {
   name: {
-    kor: '이이정',
-    eng: 'LEEJUNG',
+    kor: '리정',
+    eng: 'Leejung Lee',
   },
-  image: '',
+  image: 'Choreographer for TWICE, ITZY, Sunmi, Somi, BLACKPINK, iKON',
   birth: new Date(1998, 8, 8),
   isLeader: true,
   description: '',
@@ -34,6 +34,42 @@ const LeeJung: Dancer = {
   ]
 }
 
+const Yeojin: Dancer = {
+  name: {
+    kor: '여진',
+    eng: 'Yeojin',
+  },
+  image: '',
+  birth: new Date(1994, 10, 8),
+  description: '',
+  choreo: [],
+  link: []
+}
+
+const Isak: Dancer = {
+  name: {
+    kor: '이삭',
+    eng: 'Isak',
+  },
+  image: '',
+  birth: new Date(1997, 3, 1),
+  description: '',
+  choreo: [],
+  link: []
+}
+
+const Jihyo: Dancer = {
+  name: {
+    kor: '지효',
+    eng: 'Jihyo',
+  },
+  image: '',
+  birth: new Date(1996, 10, 21),
+  description: '',
+  choreo: [],
+  link: []
+}
+
 const YGX: Crew = {
   name: {
     kor: '와이지엑스',
@@ -43,6 +79,20 @@ const YGX: Crew = {
   description: '',
   finalRank: 5,
   members: [
-
+    Leejung,
+    Yeojin,
+    Yell,
+    Isak,
+    Jihyo,
   ],
 }
+
+export const YGXMember = {
+  Leejung,
+  Yeojin,
+  Yell,
+  Isak,
+  Jihyo,
+} 
+
+export default YGX
