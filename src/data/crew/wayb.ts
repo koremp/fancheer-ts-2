9768b68fc55fa525262045697059526773202ed4
Dokyun Lim <im.koremp@gmail.com>
2022-01-07 17:30:09 +0900

    Add WAYB Crew Data

--- src/data/crew/wayb.ts
+++ src/data/crew/wayb.ts
@@ -1,2 +1,90 @@
 import Crew from 'data/type/Crew'
 import Dancer from 'data/type/Dancer'
+
+const Noze: Dancer = {
+  name: {
+    kor: '노제',
+    eng: 'Noze',
+  },
+  image: '',
+  birth: new Date(1996, 2, 11),
+  isLeader: true,
+  description: `Dancer for Kai's "Mmmh", Taemin's "Advice"`,
+  choreo: [],
+  link: [],
+}
+
+const GyuriAn: Dancer = {
+  name: {
+    kor: '규리안',
+    eng: 'Gyuri An',
+  },
+  image: '',
+  birth: new Date(2002, 1, 5),
+  description: '',
+  choreo: [],
+  link: [],
+}
+
+const Dolla: Dancer = {
+  name: {
+    kor: '돌라',
+    eng: 'Dolla',
+  },
+  image: '',
+  birth: new Date(1996, 1, 14),
+  description: '',
+  choreo: [],
+  link: [],
+}
+
+const LeeSu: Dancer = {
+  name: {
+    kor: '리수',
+    eng: 'Lee Su',
+  },
+  image: '',
+  birth: new Date(2000, 7, 0),
+  description: `Dancer for Kai's "Mmmh" (with Noze), and The Boyz`,
+  choreo: [],
+  link: [],
+}
+
+const Ansso: Dancer = {
+  name: {
+    kor: '안쏘',
+    eng: 'Ansso',
+  },
+  image: '',
+  birth: new Date(1997, 8, 0),
+  description: '',
+  choreo: [],
+  link: [],
+}
+
+const WAYB: Crew = {
+  name: {
+    kor: '웨이비',
+    eng: 'WAYB',
+  },
+  image: '',
+  description: '',
+  finalRank: 8,
+  members: [
+    Noze,
+    GyuriAn,
+    Dolla,
+    LeeSu,
+    Ansso,
+  ],
+}
+
+export const WaybMembers = {
+  Noze,
+  GyuriAn,
+  Dolla,
+  LeeSu,
+  Ansso,
+}
+
+export default WAYB
