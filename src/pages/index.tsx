69d3088c109b259a44dae5378fb5471d582cfcec
Dokyun Lim <im.koremp@gmail.com>
2022-01-10 15:00:52 +0900

    Rename Members to Dancers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,35 +3,41 @@ import { Layout, Menu } from 'antd'
 import produce from 'immer';
 
 import data from 'datas/crews'
-import CREW from 'datas/types/Crew'
+import Crew from 'datas/types/Crew'
+import Dancer from 'datas/types/Dancer'
 
 const { Header, Content, Sider, Footer } = Layout
 
 const Main = () => {
-  const [crew, setCrew] = useState<CREW>(data.crews.find((crew) => crew.name.eng === data.key))
+  const [crew, setCrew] = useState<Crew>(data.crews.find((crew) => crew.name.eng === data.key))
   const [crewKey, setCrewKey] = useState<string>(data.key)
+
+  const [dancer, setDancer] = useState<Dancer>(crew.dancers.find((dancer) => dancer.name.eng === crew.key))
 
   const onClickCrew = (e) => {
     setCrewKey(e.key)
     console.log(crewKey, e.key)
+  }
+  const onClickDancer = (e) => {
+    setCrew(produce(crew, draft => {
+      draft.key = e.key
+    }))
   }
   
   useEffect(() => {
     setCrew(data.crews.find((crew) => crew.name.eng === crewKey))
   }, [crewKey])
 
-  const onClickMember = (e) => {
-    // setCrew(produce(crew, draft => {
-    //   draft[draft.key].index = e.key
-    // }))
-  }
+  useEffect(() => {
+    setDancer(crew.dancers.find((dancer) => dancer.name.eng === crew.key))
+  }, [crew.key])
 
   return (
     <Layout >
       <Header>
         <Menu theme="dark" mode="horizontal" onClick={onClickCrew}>
           {
-            data.crews.map((crew, index) => (
+            data.crews.map((crew) => (
               <Menu.Item key={crew.name.eng}>{crew.name.eng}</Menu.Item>
             ))
           }
@@ -39,17 +45,18 @@ const Main = () => {
       </Header>
       <Layout hasSider>
         <Sider>
-          <Menu mode="inline" >
+          <Menu mode="inline" onClick={onClickDancer} selectedKeys={[crew.key]}>
             {
-              crew.members.map((member) => (
-                <Menu.Item key={member.name.eng}>{member.name.eng}</Menu.Item>
+              crew.dancers.map((dancer) => (
+                <Menu.Item key={dancer.name.eng}>{dancer.name.eng}</Menu.Item>
               ))
             }
           </Menu>
         </Sider>
         <Layout>
           <Content>
-            Content
+            {dancer.name.eng}
+            {dancer.name.kor}
           </Content>
           <Footer>
             Footer
