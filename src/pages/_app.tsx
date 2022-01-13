0db516bdb15486bb4e458272b7646f193ab292f8
Dokyun Lim <im.koremp@gmail.com>
2022-01-13 21:18:50 +0900

    Fix Head of Next App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
-import { Layout } from 'antd'
 import { Global } from '@emotion/react'
 import { GoogleFonts } from 'next-google-fonts'
 import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 
 import 'antd/dist/antd.css'
 
-const App = ({ Component, pageProps}) => {
+const App = ({ Component, pageProps }) => {
   return (
-    <Layout id='__next'>
+    <>
       <DefaultSeo
         title="FanCheer"
         description="FanCheer Version 2 With TypeScript, Next.js"
@@ -16,7 +16,6 @@ const App = ({ Component, pageProps}) => {
           url: 'https://koremp.github.io/fancheer-ts-2',
           // images: [{
           //     url: `https://koremp.github.io/fancheer-ts-2`,
-          //     alt: '이상한 나라의 성범죄 사건들. 당신의 판결은?'
           // }]
         }}
       />
@@ -36,16 +35,17 @@ const App = ({ Component, pageProps}) => {
           },
           'ol, ul, li': {
             listStyle: 'none'
-          },
-          '#__next': {
-            position: 'absolute',
-            height: '100vh',
-            width: '100vw',
           }
         }}
       />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
       <Component {...pageProps} />
-    </Layout>
+    </>
   )
 }
 
