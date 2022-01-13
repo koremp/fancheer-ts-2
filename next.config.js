bd788fcd82029b060c287f04e86eefbddd00febf
Dokyun Lim <im.koremp@gmail.com>
2022-01-13 12:31:54 +0900

    Add assets to Path Alias in Next Config

--- next.config.js
+++ next.config.js
@@ -8,13 +8,13 @@ let config = {
             // utils: path.join(__dirname, 'src', 'utils'),
             // hooks: path.join(__dirname, 'src', 'hooks'),
             // styles: path.join(__dirname, 'src', 'styles'),
-            // assets: path.join(__dirname, 'src', 'assets'),
+            assets: path.join(__dirname, 'src', 'assets'),
             components: path.join(__dirname, 'src', 'components'),
             pages: path.join(__dirname, 'src', 'pages'),
             datas: path.join(__dirname, 'src', 'datas'),
         };
         return config;
-    },
+    }
 }
 
 module.exports = withImages(config)
