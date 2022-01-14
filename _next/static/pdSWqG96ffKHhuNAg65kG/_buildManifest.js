42ddd7a6b7519b19480d61ace0e457ad5a3eb754
koremp <im.koremp@gmail.com>
2022-01-14 05:35:11 +0000

    Deploying to gh-pages from @ koremp/fancheer-ts-2@be8f09d513df121285d4fd87ffb89cdcb166dc9f 🚀

--- _next/static/pdSWqG96ffKHhuNAg65kG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-4d2f0a9f494a9dad.js","static/chunks/305-861326a30fd893bf.js","static/chunks/pages/index-dbb7c857df971096.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
