ad90b954a1d918392e39a5f779694c37f2d1859d
Dokyun Lim <im.koremp@gmail.com>
2022-01-21 16:09:07 +0900

    Fix reducer import

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import dancerCrewSlice from 'features/dancerCrew/dancerCrewSlice'
+import reducer from 'features/dancerCrew/dancerCrewSlice'
 
 export const store = configureStore({
-  reducer: reducer,
+  reducer,
   devTools: process.env.NODE_ENV !== "production"
 })
