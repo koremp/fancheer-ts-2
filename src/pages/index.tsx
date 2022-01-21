f4fa9e2c9117d4ab4d9195a100da9800a1837821
Dokyun Lim <im.koremp@gmail.com>
2022-01-21 16:09:33 +0900

    Correct dancer and crew data format in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,10 +35,10 @@ const Main = () => {
   return (
     <Layout>
       <Header>
-        <Menu theme="dark" mode="horizontal" onClick={onClickCrew} selectedKeys={[choosenCrew.name.eng]}>
+        <Menu theme="dark" mode="horizontal" onClick={onClickCrew} selectedKeys={[crew.name.eng]}>
           {
-            crewName.map((crew) => (
-              <Menu.Item key={crew.name.eng}>{crew.name.eng}</Menu.Item>
+            crewName.map(({name}) => (
+              <Menu.Item key={name.eng}>{name.eng}</Menu.Item>
             ))
           }
         </Menu>
@@ -47,8 +47,8 @@ const Main = () => {
         <Sider style={{ background: "#fff" }}>
           <Menu mode="inline" onClick={onClickDancer} selectedKeys={[choosenDancer.name.eng]}>
             {
-              dancerName.map((dancer) => (
-                <Menu.Item key={dancer.name.eng}>{dancer.name.eng}</Menu.Item>
+              dancerName.map(({name}) => (
+                <Menu.Item key={name.eng}>{name.eng}</Menu.Item>
               ))
             }
           </Menu>
@@ -57,16 +57,16 @@ const Main = () => {
           <Layout style={{ display: 'flex', flexDirection: 'column', padding: '24px', margin: 0, background: "#fff" }}>
             <Content>
               <Image 
-                src={selectedDancer.image} 
-                alt={`${selectedDancer.name.eng}'s photo`} 
-                width={selectedDancer.image.width / 2}
-                height={selectedDancer.image.height / 2} 
+                src={choosenDancer.image} 
+                alt={`${choosenDancer.name.eng}'s photo`} 
+                width={choosenDancer.image.width / 2}
+                height={choosenDancer.image.height / 2} 
               />
               <Descriptions title="Dancer Info" style={{ margin: 'auto 0 0 auto' }} column={1}>
-                <Descriptions.Item label="Dancer Name (kor/eng)">{dancer.name.kor} / {dancer.name.eng}</Descriptions.Item>
-                <Descriptions.Item label="Leader">{selectedDancer.isLeader ? '리더' : '-'}</Descriptions.Item>
-                <Descriptions.Item label="Age">{moment(selectedDancer.birth).format('YYYY-MM-DD')} / {moment().diff(dancer.birth.toString(), 'years')}</Descriptions.Item>
-                <Descriptions.Item label="Descriptions">{selectedDancer.description}</Descriptions.Item>
+                <Descriptions.Item label="Dancer Name (kor/eng)">{choosenDancer.name.kor} / {choosenDancer.name.eng}</Descriptions.Item>
+                <Descriptions.Item label="Leader">{choosenDancer.isLeader ? '리더' : '-'}</Descriptions.Item>
+                <Descriptions.Item label="Age">{moment(choosenDancer.birth).format('YYYY-MM-DD')} / {moment().diff(choosenDancer.birth.toString(), 'years')}</Descriptions.Item>
+                <Descriptions.Item label="Descriptions">{choosenDancer.description}</Descriptions.Item>
               </Descriptions>
             </Content>
           </Layout>
