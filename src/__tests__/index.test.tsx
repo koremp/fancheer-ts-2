cd484ae7800e32483698901c640db55a39b3bf2f
Dokyun Lim <im.koremp@gmail.com>
2022-01-22 16:06:43 +0900

    Update main index page

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -2,29 +2,7 @@ import { render } from '@testing-library/react';
 
 import Main from 'pages/index';
 
-describe("Main", () => {
-  beforeEach(() => {
-    Object.defineProperty(window, 'matchMedia', {
-      writable: true,
-      value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // Deprecated
-        removeListener: jest.fn(), // Deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-      }))
-    })
-  })
-
-  test('Selected Crew changes when header menu key is changed.', () => {
-    render(<Main/>)
-  })
-
-  test('Selected Dancer changes when sider menu key is changed.', () => {
-    render(<Main/>)
-  })
+test('Main.', () => {
+  render(<Main/>)
 })
 
