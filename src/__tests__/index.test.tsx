29eafbb183f5fe8f1a7d975b8eca97df3b7b6f39
Dokyun Lim <im.koremp@gmail.com>
2022-01-22 19:00:48 +0900

    Set up a test main

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,8 +1,29 @@
-import { render } from '@testing-library/react';
 
-import Main from 'pages/index';
+import { useDispatch, useSelector } from 'react-redux'
+import { render } from '@testing-library/react'
 
-test('Main.', () => {
-  render(<Main/>)
+import Main from 'pages/index'
+
+import { initialState } from 'features/dancerCrew/dancerCrewSlice'
+
+jest.mock('react-redux')
+
+
+
+describe('Main', () => {
+  const dispatch = jest.fn()
+
+  beforeEach(() => {
+    dispatch.mockClear()
+
+    useDispatch.mockImplementation(() => dispatch)
+
+    useSelector.mockImplementation((selector) => selector(
+      initialState
+    ))
+  })
+
+  it('renders Main', () => {
+    render(<Main/>)
+  })
 })
-
