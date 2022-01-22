856a61fc556aa496322f7d2dcf69eece1bbafb1f
Dokyun Lim <im.koremp@gmail.com>
2022-01-22 19:00:26 +0900

    Add initial crew slice to dancer crew slice test

--- src/features/dancerCrew/dancerCrewSlice.test.ts
+++ src/features/dancerCrew/dancerCrewSlice.test.ts
@@ -1,44 +1,36 @@
-import reducer, { chooseCrew, chooseDancer, DancerCrewState } from './dancerCrewSlice'
+import reducer, { chooseCrew, chooseDancer, initialState, DancerCrewState } from './dancerCrewSlice'
 
 import data from 'datas/crews'
 
 const { crews } = data
 
 describe('Dancer Crew Slice', () => {
-  const previousState: DancerCrewState = {
-    crews,
-    crewKey: crews[0].name.eng,
-    crew: crews[0],
-    dancerKey: crews[0].dancers[0].name.eng,
-    dancer: crews[0].dancers[0]
-  }
-
   it('returns initialState', () => {
     const state = reducer(undefined, { type: 'action' });
 
-    expect(state).toEqual(previousState);
+    expect(state).toEqual(initialState);
   })
 
   describe('Choose Crew', () => {
     it('should handle changing crew and dancer key', () => {
-      const state = reducer({...previousState}, chooseCrew(crews[1].name.eng))
+      const state = reducer({...initialState}, chooseCrew(crews[1].name.eng))
 
       expect(state).toEqual({
-        ...previousState,
+        ...initialState,
         crewKey: crews[1].name.eng,
         crew: crews[1],
-        dancerKey: crews[1].key,
-        dancer: crews[1].dancers.find((dancer) => dancer.name.eng === crews[1].key)
+        dancerKey: crews[1].dancers[0].name.eng,
+        dancer: crews[1].dancers[0]
       })
     })  
   })
 
   describe('Choose Dancer', () => {
     it('should handle changing dancer key', () => {
-      const state = reducer({...previousState}, chooseDancer(crews[0].dancers[3].name.eng))
+      const state = reducer({...initialState}, chooseDancer(crews[0].dancers[3].name.eng))
 
       expect(state).toEqual({
-        ...previousState,
+        ...initialState,
         dancerKey: crews[0].dancers[3].name.eng,
         dancer: crews[0].dancers[3],
       })
