f2283eb78e6e64919bf1e1bcda0b0885315a7b8a
Dokyun Lim <im.koremp@gmail.com>
2022-01-24 19:19:32 +0900

    Move root reducer to feature store

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from 'features/dancerCrew/dancerCrewSlice'
 
-const store = configureStore({
-  reducer,
-  devTools: process.env.NODE_ENV !== "production"
-})
+const store = configureStore({ reducer })
 
 export default store
