50432023c7bae706e4ce4927995719d20c1c01da
Dokyun Lim <im.koremp@gmail.com>
2022-01-29 19:13:31 +0900

    Change dancer image and descriptions to horizontal view

--- src/components/dancerContent/index.tsx
+++ src/components/dancerContent/index.tsx
@@ -11,23 +11,28 @@ const DancerContent = () => {
   const choosenDancer = useSelector(dancer)
 
   return (
-    <Content>
-      <Image 
-        src={choosenDancer.image} 
-        alt={`${choosenDancer.name.eng}'s photo`} 
-        width={choosenDancer.image.width / 2}
-        height={choosenDancer.image.height / 2} 
-      />
-      <Descriptions title="Dancer Info" style={{ margin: 'auto 0 0 auto' }} column={1}>
-        <Descriptions.Item label="Dancer Name (kor/eng)">{choosenDancer.name.kor} / {choosenDancer.name.eng}</Descriptions.Item>
-        {
-          choosenDancer.isLeader &&
-            <Descriptions.Item label="Leader">리더</Descriptions.Item>
-        }
-        <Descriptions.Item label="Age">{moment(choosenDancer.birth).format('YYYY-MM-DD')} / {moment().diff(choosenDancer.birth.toString(), 'years')}</Descriptions.Item>
-        <Descriptions.Item label="Descriptions">{choosenDancer.description}</Descriptions.Item>
-      </Descriptions>
-    </Content>
+    <Layout style={{display: 'flex', flexDirection: 'row' }}>
+      <Content style={{ flexBasis: '40%', padding: '10px' }}>
+        <Image 
+          src={choosenDancer.image} 
+          alt={`${choosenDancer.name.eng}'s photo`}
+        />
+      </Content>
+      <Content style={{ flexBasis: '40%' }}>
+        <Descriptions 
+          title="Dancer Info" 
+          column={1}
+        >
+          <Descriptions.Item label="Dancer Name (kor/eng)">{choosenDancer.name.kor} / {choosenDancer.name.eng}</Descriptions.Item>
+          {
+            choosenDancer.isLeader &&
+              <Descriptions.Item label="Leader">리더</Descriptions.Item>
+          }
+          <Descriptions.Item label="Age">{moment(choosenDancer.birth).format('YYYY-MM-DD')} / {moment().diff(choosenDancer.birth.toString(), 'years')}</Descriptions.Item>
+          <Descriptions.Item label="Descriptions">{choosenDancer.description}</Descriptions.Item>
+        </Descriptions>
+      </Content>
+    </Layout>
   )
 }
 
